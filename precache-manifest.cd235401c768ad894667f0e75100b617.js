0df8a13575da884c990d1655513bd302121482f9
calipso28 <andres-severien@hotmail.de>
2021-03-31 11:48:18 +0200

    Updates

---
+++ precache-manifest.cd235401c768ad894667f0e75100b617.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12c7e8937cf0bd457f6234e3c25a35c8",
+    "url": "/Jamming-React-App/index.html"
+  },
+  {
+    "revision": "791844bad268b332bfa1",
+    "url": "/Jamming-React-App/static/css/main.2df9c73e.chunk.css"
+  },
+  {
+    "revision": "3303c47f2e5c622c2f18",
+    "url": "/Jamming-React-App/static/js/2.f566f6b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Jamming-React-App/static/js/2.f566f6b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "791844bad268b332bfa1",
+    "url": "/Jamming-React-App/static/js/main.001b69f4.chunk.js"
+  },
+  {
+    "revision": "db8f948e91735a07fedc",
+    "url": "/Jamming-React-App/static/js/runtime-main.a37dbd02.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/Jamming-React-App/static/media/background_photo_desktop.37be7357.jpg"
+  },
+  {
+    "revision": "7988bc0dd42ebedaf5865c4ff7156317",
+    "url": "/Jamming-React-App/static/media/checked.7988bc0d.png"
+  }
+]);
